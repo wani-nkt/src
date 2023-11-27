e82dd0116f6d2b8c3c636e229e21876aaa49553c
wani-nkt <wani.ctf.nkt@gmail.com>
2023-11-27 23:54:47 +0900

    基本的な機能の実装

--- app.vue
+++ app.vue
@@ -1,5 +1,50 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <main class="">
+    <h1 class="text-3xl font-bold p-5 flex justify-center">
+      ToDoアプリ
+    </h1>
+    <section class="">
+      <h2 class="flex justify-center">Todo List</h2>
+      <div class="flex justify-center">
+        <ul>
+          <li v-for="todo in todoStore.todoList" :key="todo.id">
+            <input type="checkbox" v-model="todo.isDone" @change="completeTodo(todo.id)">
+            {{ todo.name }}
+            <button @click="deleteTodo(todo.id)">削除</button>
+          </li>
+          <li>
+            <input v-model="newTodo" class=" border border-black">
+            <button @click="addTodo">追加</button>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </main>
 </template>
+
+<script setup lang="ts">
+import { useTodoStore } from '@/store/todo'
+
+const todoStore = useTodoStore()
+
+const newTodo = ref<string>('')
+
+const addTodo = () => {
+  if (newTodo.value === '') {
+    // Todo: alertを出す
+  } else {
+    todoStore.add(newTodo.value)
+    newTodo.value = ''
+  }
+}
+
+const completeTodo = (id: string) => {
+  todoStore.complete(id)
+}
+
+const deleteTodo = (id: string) => {
+  todoStore.delete(id)
+}
+
+
+</script>
---
+++ store/todo.ts
@@ -0,0 +1,36 @@
+import { defineStore } from 'pinia'
+import { v4 as uuidv4 } from 'uuid'
+
+interface Todo {
+    id: string,
+    name: string,
+    isDone: boolean,
+}
+
+export const useTodoStore = defineStore('todoStore', {
+    state: () => ({
+        todoList: [] as Todo[]
+    }),
+    actions: {
+        add(todoName: string) {
+            const id = uuidv4()
+            this.todoList.push({
+                id: id,
+                name: todoName,
+                isDone: false
+            })
+        },
+        complete(id: string) {
+            this.todoList.forEach(todo => {
+                if (todo.id == id) {
+                    todo.isDone = !todo.isDone
+                }
+            })
+        },
+        delete(id: string) {
+            const index = this.todoList.findIndex(todo => todo.id == id)
+            this.todoList.splice(index, 1)
+
+        }
+    }
+})
